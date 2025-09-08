a5fef8b7f0ec1a3236dc43b3650ed8b382269c66
Tsionalemu <tsionalemuta@gmail.com>
2025-09-08 03:02:07 +0300

    Add subquery SQL examples for Airbnb database

--- database-adv-script/README.md
+++ database-adv-script/README.md
@@ -4,3 +4,10 @@ This file contains SQL queries for the Airbnb database:
 
 1. Count total bookings per user.
 2. Rank properties based on the total number of bookings using a window function (ROW_NUMBER).
+
+# Subqueries
+
+This file contains SQL queries using subqueries in the Airbnb database:
+
+1. Find properties where the average rating is greater than 4.0 (non-correlated subquery).
+2. Find users who have made more than 3 bookings (correlated subquery).
--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -16,3 +16,22 @@ WHERE (
     FROM bookings b
     WHERE b.user_id = u.id
 ) > 3;
+
+-- Non-correlated subquery: Properties with average rating greater than 4.0
+SELECT *
+FROM properties
+WHERE property_id IN (
+    SELECT property_id
+    FROM reviews
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
+
+-- Correlated subquery: Users who have made more than 3 bookings
+SELECT *
+FROM users u
+WHERE (
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.user_id
+) > 3;
